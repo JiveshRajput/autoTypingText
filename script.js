1519e134703766c6ba46e469fa850f9dd88bf24b
jivesh <jsoperatorz@gmail.com>
2022-05-02 19:49:05 +0530

    Adding Auto Typing Text Project

---
+++ script.js
@@ -0,0 +1,8 @@
+let typed = new Typed("#autoType",{
+    strings: ["Eating", "Coding", "Learning"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 50,
+    loop: true,
+    loopCount: Infinity
+})
